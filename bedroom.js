cd5ffa3e8b15bb95b8436f047ae16c17e035e0b0
Swara Doiphode <85820539+Swara31@users.noreply.github.com>
2022-12-25 15:02:12 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,22 @@
+img = "";
+status = "";
+
+
+
+function back()
+{
+    window.location="index.html";
+}
+
+function preload()
+{
+    img = loadImage('b3-image.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+}
+
---
+++ main.js
@@ -0,0 +1,27 @@
+
+function button_1()
+{
+    window.location="garden.html";
+}
+
+function button_2()
+{
+    window.location="kitchen.html";
+}
+
+function button_3()
+{
+    window.location="bedroom.html";
+}
+
+function button_4()
+{
+    window.location="studytable.html";
+}
+
+function button_5()
+{
+    window.location="mall.html";
+}
+
+
